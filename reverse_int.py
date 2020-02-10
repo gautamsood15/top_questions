45dc210262b5e1a6d31e200997210d80f2edb2cd
gautamsood15 <gauty22@gmail.com>
2020-02-10 05:44:02 +0530

    reverse int complete

---
+++ reverse_int.py
@@ -0,0 +1,17 @@
+
+
+def reverse_integer(n):
+
+	reversed=0
+	remainer=0
+
+	while n>0:
+		remainer=n%10
+		n=n//10
+		reversed=reversed*10+remainer
+
+	return reversed
+
+if __name__ == "__main__":
+
+	print(reverse_integer(4321))
