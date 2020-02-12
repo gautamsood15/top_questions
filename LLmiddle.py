154bfcc9694d23e8e9a38ad36b6370d48a65ac14
gautamsood15 <gauty22@gmail.com>
2020-02-13 03:06:11 +0530

    linked list classes

---
+++ LLmiddle.py
@@ -0,0 +1,13 @@
+class Node(object):
+
+	def __init__(self, data):
+		self.data = data
+		self.nextNode = None
+
+
+class LinkedList(object):
+
+	def __init__(self):
+		self.head = None
+		self.size = 0
+
