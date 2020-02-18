5ebf4d98ec0b2ecbba6131a56226989d13dc67ac
gautamsood15 <gauty22@gmail.com>
2020-02-18 05:32:00 +0530

    Ädding BST code in bst_comparator

--- bst_comparator.py
+++ bst_comparator.py
@@ -23,6 +23,115 @@ class BinarySearchTree(object):
 	def __init__(self):
 		self.root = None
 
+	def insert(self, data):
+		if not self.root:
+			self.root = Node(data)
+
+		else:
+			self.insertNode(data, self.root)
+
+	def insertNode(self, data, node):
+
+		if data < node.data:
+			if node.leftChild:
+				self.insertNode(data, node.leftChild)
+
+			else:
+				node.leftChild = Node(data)
+
+		else:
+			if node.rightChild:
+				self.insertNode(data, node.rightChild)
+
+			else:
+				node.rightChild = Node(data)
+
+
+	def remove(self, data):
+		if self.root:
+			self.root = self.removeNode(data, self.root)
+
+	def removeNode(self, data, node):
+		if not node:
+			return node
+
+		if data < node.data:
+			node.leftChild = self.removeNode(data, node.leftChild)
+
+		elif data > node.data:
+			node.rightChild = self.removeNode(data, node.rightChild)
+
+		else:
+
+			if not node.leftChild and not node.rightChild:
+				print("Removing a leaf node....")
+				del node
+				return None
+
+			if not node.leftChild:
+				print("Removing node with single right child ")
+
+				tempNode = node.rightChild
+				del node
+				return tempNode
+
+			elif not node.rightChild:
+				print("Removing node with single left child ")
+
+				tempNode = node.leftChild
+				del node
+				return tempNode
+
+			print("Removing node with 2 children")
+
+			tempNode = self.getPredecessor(node.leftChild)
+			node.data = tempNode.data
+			node.leftChild = self.removeNode(tempNode.data, node.leftChild)
+
+		return node
+
+
+	def getPredecessor(self, node):
+
+		if node.rightChild:
+			return self,getPredecessor(node.rightChild)
+
+		return node
+
+
+	def getMinValue(self):
+		if self.root:
+			return self.getMin(self.root)
+
+	def getMin(self, node):
+		if node.leftChild:
+			return self.getMin(node.leftChild)
+
+		return node.data
+
+	def getMaxValue(self):
+		if self.root:
+			return self.getMax(self.root)
+
+	def getMax(self, node):
+		if node.rightChild:
+			return self.getMax(node.rightChild)
+
+		return node.data
+
+	def traverse(self):
+		if self.root:
+			self.traverseInOrder(self.root)
+
+	def traverseInOrder(self, node):
+
+		if node.leftChild:
+			self.traverseInOrder(node.leftChild)
+
+		print("%s" % node.data)
+
+		if node.rightChild:
+			self.traverseInOrder(node.rightChild)
 
 
 if __name__ == "__main__":
